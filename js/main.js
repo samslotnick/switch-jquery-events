ae06e131ef6789aea0156a669b21a2a81986f391
Sam Slotnick <samslotnick@Sams-MacBook-Pro.local>
2017-05-01 15:30:15 -0400

    complete xcpt text toggle

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 
-
+//
+// var dark = $('button');
+// dark.attr("class", "switch on");
+//1
+$("button").click(function(){
+  $("body").toggleClass("dark");
+  $("h1").toggleClass("dark");
+  $("h1").text("Hey, who turned off the lights?");
+});
